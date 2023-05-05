24aac1e4466d19f6523ed2d12da4a8c1116acc2e
DanMark <daniel.mark.kiss@gmail.com>
2023-05-05 11:16:35 +0200

    feat: :sparkles: init

---
+++ iSoccer/iSoccer Watch App/iSoccerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSoccerApp.swift
+//  iSoccer Watch App
+//
+//  Created by Kiss Dániel Márk on 2023. 05. 05..
+//
+
+import SwiftUI
+
+@main
+struct iSoccer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iSoccer/iSoccer/iSoccerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSoccerApp.swift
+//  iSoccer
+//
+//  Created by Kiss Dániel Márk on 2023. 05. 05..
+//
+
+import SwiftUI
+
+@main
+struct iSoccerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
